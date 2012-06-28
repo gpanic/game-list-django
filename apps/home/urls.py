90dfe0685bad9b5ba1f92d7f82fb79ae3c19d81f
Gregor Panič <vision810@gmail.com>
2012-06-28 19:18:43 +0200

    redirect home to games index

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-	url(r'^$',
-		TemplateView.as_view(
-			template_name='home/index.html'),
-		name="home_index"),
+	# url(r'^$',
+	# 	TemplateView.as_view(
+	# 		template_name='home/index.html'),
+	# 	name="home_index"),
+	url(r'^$', 'apps.home.views.home_index', name='home_index'),
 )
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+from django.template import RequestContext
+
+def home_index(request):
+	return redirect(reverse('games_game_index'), context_instance=RequestContext(request))
